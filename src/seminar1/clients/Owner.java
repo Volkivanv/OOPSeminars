690d859ce3f4a0e27b6239e9d7e7787b9c059fa0
Ivan <volkivanv@mail.ru>
2024-01-28 15:50:18 +0300

    Init commit

---
+++ src/seminar1/clients/Owner.java
@@ -0,0 +1,18 @@
+package seminar1.clients;
+
+
+public class Owner {
+    String fullName;
+
+    public Owner(String fullName) {
+        this.fullName = fullName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "fullName='" + fullName + '\'' +
+                '}';
+    }
+}
